3784df7b735b043381cd5349a9ec82eee86c18b2
Pedro Manuel Díaz Santa María <pedro@diazsantamaria.com>
2017-04-22 16:36:43 -0500

    Fixed

--- database/seeds/ChaptersThesis.php
+++ database/seeds/ChaptersThesis.php
@@ -13,18 +13,18 @@ class ChaptersThesis extends Seeder
      */
     public function run()
     {
-        ChapterThesis::create(['name'=>'numeros romanos'  ,'number'=>1,'thesis_id'=>1]);
-        ChapterThesis::create(['name'=>'numeros reales'   ,'number'=>2,'thesis_id'=>1]);
-        ChapterThesis::create(['name'=>'numeros complejos','number'=>3,'thesis_id'=>1]);
-        ChapterThesis::create(['name'=>'limites'          ,'number'=>4,'thesis_id'=>1]);
-        ChapterThesis::create(['name'=>'metodos numericos','number'=>5,'thesis_id'=>1]);
-        ChapterThesis::create(['name'=>'derivadas'        ,'number'=>6,'thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'numeros romanos'  ,'thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'numeros reales'   ,'thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'numeros complejos','thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'limites'          ,'thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'metodos numericos','thesis_id'=>1]);
+        ChapterThesis::create(['content'=>'derivadas'        ,'thesis_id'=>1]);
 
-        ChapterThesis::create(['name'=>'Nacimiento, Hijo de satan'  ,'number'=>1,'thesis_id'=>2]);
-        ChapterThesis::create(['name'=>'Muerte de Shiro'            ,'number'=>2,'thesis_id'=>2]);
-		ChapterThesis::create(['name'=>'El nuevo exorcista'         ,'number'=>3,'thesis_id'=>2]);
-		ChapterThesis::create(['name'=>'El gato de Rin'             ,'number'=>4,'thesis_id'=>2]);
-		ChapterThesis::create(['name'=>'Enfrentamiento Yukio'       ,'number'=>5,'thesis_id'=>2]);
-		ChapterThesis::create(['name'=>'Tragado por su padre, satan','number'=>6,'thesis_id'=>2]);
+        ChapterThesis::create(['content'=>'Nacimiento, Hijo de satan'  ,'thesis_id'=>2]);
+        ChapterThesis::create(['content'=>'Muerte de Shiro'            ,'thesis_id'=>2]);
+		ChapterThesis::create(['content'=>'El nuevo exorcista'         ,'thesis_id'=>2]);
+		ChapterThesis::create(['content'=>'El gato de Rin'             ,'thesis_id'=>2]);
+		ChapterThesis::create(['content'=>'Enfrentamiento Yukio'       ,'thesis_id'=>2]);
+		ChapterThesis::create(['content'=>'Tragado por su padre, satan','thesis_id'=>2]);
     }
 }
