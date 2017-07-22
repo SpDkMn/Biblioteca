90d1edd80229a401ce5b845ff6446a6909de69f3
vagrant <vagrant@homestead>
2017-07-22 16:58:38 +0000

    m

--- database/migrations/2017_02_11_194717_create_editorials_table.php
+++ database/migrations/2017_02_11_194717_create_editorials_table.php
@@ -17,7 +17,7 @@ class CreateEditorialsTable extends Migration
 
             $table->increments('id');
             $table->string('name');
-        
+
             $table->timestamps();
             $table->softDeletes();
         });
