79266766532d80677dd946c7dc68c4e9ba9137c2
luisquispetaquire <luisquispetaquire@gmail.com>
2017-08-19 02:18:17 -0500

    mejorando modal terminado

--- resources/views/user/md_orders/tableBooks.blade.php
+++ resources/views/user/md_orders/tableBooks.blade.php
@@ -105,34 +105,8 @@
                       <h3 class="box-title text-center" id="book_title"></h3>
                     </div>
                     <div class="box-body  no-padding">
-                      <table class="table table-hover table-striped table-bordered">
-                          <tr>
-                            <th class="itemTable text-center">Numero de ingreso</th>
-                            <th class="itemTable text-center">Codigo de barras</th>
-                            <th class="itemTable text-center">Ejemplar</th>
-                            <th class="itemTable text-center">Volumen</th>
-                            <th class="itemTable text-center">Clasificacion</th>
-                            <th class="itemTable text-center">Estado</th>
-                            <th class="itemTable text-center">Pedir</th>
-                          </tr>
-                      <!--    @foreach($book->bookCopies as $copy)
-                          <tr @if($copy->
-                            availability) class="success" @else class="danger" @endif>
-                            <td class="text-center">{{$copy->incomeNumber}}</td>
-                            <td class="text-center">{{$copy->barcode}}</td>
-                            <td class="text-center">{{$copy->copy}} @if($copy->availability) <span
-                              class="sr-only">Disponible</span> @else <span class="sr-only">No
-                                disponible</span> @endif
-                            </td>
-                            <td class="text-center"><?php //if($copy->volume != "") echo ( $copy->volume); else echo "_";  ?></td>
-                            <td class="text-center">{{$copy->clasification}}</td>
-                            <td class="text-center"> @if($copy->availability) <span
-                              class="label label-info">Disponible</span>@else<span class="label label-danger">No Disponible</span>@endif</td>
-                            <form id="order_form" method="get">
-                              <td class="text-center"><button type="submit" class="btn_pedido"  name="button"><i class="fa fa-book"></i></button></td>
-                            </form>
-                          </tr>
-                        @endforeach -->
+                      <table class="table table-hover table-striped table-bordered" id="book_copies">
+                                            
                       </table>
                     </div>
                   </div>
@@ -145,15 +119,13 @@
                     <!-- ACOMODAR -->
                     <div class="subtitle" id="book_subtitle"></div>
                     <div class="box-body" id="book_summary"></div>
-                    <!--
+                    
                       <div class="subtitle">Contenido</div>
                       <ol id="book_chapters">
-                        @foreach($book->chapters as $chapter)
-                        <li>{{$chapter->name}}</li>
-                        @endforeach
+                        
                       </ol>
 
-                    -->
+                    
 
                   </div>
                 </div>
@@ -193,8 +165,16 @@
 
 <?php
 use App\Book as Book;
+use App\ChapterBook as ChapterBook;
+use App\BookCopy as BookCopy;
 	$books = Book::all();
-	$objJson = json_encode($books);
+  $chapterBooks = ChapterBook::all();
+  $bookCopies = BookCopy::all();
+
+
+	$bookJson = json_encode($books);
+  $chapterBookJson = json_encode($chapterBooks);
+  $bookCopyJson = json_encode($bookCopies);
 ?>
 
 <script type="text/javascript">
@@ -207,21 +187,116 @@ use App\Book as Book;
       $(".btn_info").on('click',function(event) {
         $id = $(this).data('id')
 
-        var books = eval(<?php echo $objJson; ?>);
+        var books = eval(<?php echo $bookJson; ?>);
+        var chapterBooks = eval(<?php echo $chapterBookJson; ?>);
+        var bookCopies = eval(<?php echo $bookCopyJson; ?>);
+        var indice;
+
         for(i=0;i<books.length;i++){
     			if(books[i].id == $id){
+            indice=i;
             break;
     			}
     		}
+        console.log("paso1");
+        console.log("paso2");
+        console.log("paso3");
+        var cont=0;
+        var chapterBook = new Array();
+        var bookCopy = new Array();
 
-        $('#book_title').html(books[i].title);
-        $('#book_subtitle').html(books[i].secondaryTitle);
-        $('#book_summary').html(books[i].summary);
-        $('#book_extension').html("Extension:"+books[i].extension);
-        $('#book_details').html("Otros detalles fisicos:"+books[i].physicalDetails)
-        $('#book_dimensions').html("Extension:"+books[i].dimensions);
-        $('#book_accompaniment').html("Material de Acompañamiento:"+books[i].accompaniment);
+        for(i=0;i<chapterBooks.length;i++){
+          if(chapterBooks[i].book_id == books[indice].id){
+            chapterBook.push(chapterBooks[i]);
+          }
+        }
+        for(i=0;i<bookCopies.length;i++){
+          if(bookCopies[i].book_id == books[indice].id){
+            bookCopy.push(bookCopies[i]);
+          }
+        }
+        for(i=0;i<bookCopy.length;i++){
+          for(j=0;j<bookCopy.length-1;j++){
+            if(bookCopy[j].copy > bookCopy[j+1].copy){
+              var aux=bookCopy[j];
+              bookCopy[j]=bookCopy[j+1];
+              bookCopy[j+1]=aux;
+            }
+          }
+        }
+        for(i=0;i<chapterBook.length;i++){
+          for(j=0;j<chapterBook.length-1;j++){
+            if(chapterBook[j].number > chapterBook[j+1].number){
+              var aux=chapterBook[j];
+              chapterBook[j]=chapterBook[j+1];
+              chapterBook[j+1]=aux;
+            }
+          }
+        }
+        var txt_chapter = "";
+        for(i=0;i<chapterBook.length;i++){
+          txt_chapter=txt_chapter+"<li>"+chapterBook[i].name+"</li>";
+        }
+        
+        $('#book_chapters').html(txt_chapter);
+        $('#book_title').html(books[indice].title);
+        $('#book_subtitle').html(books[indice].secondaryTitle);
+        $('#book_summary').html(books[indice].summary);
+        $('#book_extension').html("Extension:"+books[indice].extension);
+        $('#book_details').html("Otros detalles fisicos:"+books[indice].physicalDetails)
+        $('#book_dimensions').html("Dimensiones:"+books[indice].dimensions);
+        $('#book_accompaniment').html("Material de Acompañamiento:"+books[indice].accompaniment);
+
+        var txt_bookCopy= '<tr>'+
+                            '<th class="itemTable text-center">Numero de ingreso</th>'+
+                            '<th class="itemTable text-center">Codigo de barras</th>'+
+                            '<th class="itemTable text-center">Ejemplar</th>'+
+                            '<th class="itemTable text-center">Volumen</th>'+
+                            '<th class="itemTable text-center">Clasificacion</th>'+
+                            '<th class="itemTable text-center">Estado</th>'+
+                            '<th class="itemTable text-center">Pedir</th>'+
+                          '</tr>';
+        for(i=0;i<bookCopy.length;i++){
+          txt_bookCopy=txt_bookCopy+'<tr';
+          if(bookCopy[i].availability==true){
+            txt_bookCopy=txt_bookCopy+'class="success">';
+          }else{
+            txt_bookCopy=txt_bookCopy+'class="danger">';
+          }
+          txt_bookCopy=txt_bookCopy+'<td class="text-center">'+bookCopy[i].incomeNumber+'</td>'+
+                                    '<td class="text-center">'+bookCopy[i].barcode+'</td>'
+                                    +'<td class="text-center">'+bookCopy[i].copy+'</td>';
+          if(bookCopy[i].availability==true){
+            txt_bookCopy=txt_bookCopy+'<span class="sr-only">Disponible</span>';
+          }else{
+            txt_bookCopy=txt_bookCopy+'<span class="sr-only">No disponible</span>';
+          }
+
+          txt_bookCopy=txt_bookCopy+'</td><td class="text-center">';
+
+          if (bookCopy[i].volume !=null) {
+            txt_bookCopy=txt_bookCopy+bookCopy[i].volume;
+          }else{
+            txt_bookCopy=txt_bookCopy+"-";
+          }
+
+          txt_bookCopy=txt_bookCopy+'</td><td class="text-center">'+bookCopy[i].clasification+'</td><td class="text-center">';
+
+          if(bookCopy[i].availability){
+            txt_bookCopy=txt_bookCopy+'<span class="label label-info">Disponible</span>';
+          }else{
+            txt_bookCopy=txt_bookCopy+'<span class="label label-danger">No Disponible</span>';
+          }
+
+          txt_bookCopy=txt_bookCopy+'</td><form id="order_form" method="get"><td class="text-center"><button type="submit" class="btn_pedido"  name="button"><i class="fa fa-book"></i></button></td></form></tr>';
+        }
+
+        $('#book_copies').html(txt_bookCopy);
+
       });
 
     });
   </script>
+
+
+                     
