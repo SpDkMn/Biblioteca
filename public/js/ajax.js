4c3ba5cfa895df61665b0a001f381337b515e4b8
Jose Mateo Carrasco <locutermo@gmail.com>
2017-08-19 17:27:50 +0000

    Arreglano errores en el filtro

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -14,41 +14,35 @@ $(function(){
 
 
 	//Cada vez que se selecciona un filtro , lo enviará al controlador
-	$('#orderCategory').change(function(){
-		var envio = $('#orderCategory').val();
 
-		$.ajax({
-			type: "post",
-			url: '/user/search',
-			// url: '/user/order',
-			data: {
-            	searchType: envio
-       		 },
-			success: function(resp){
-				if(resp!=""){
-					$('#resultados').html(resp);
+
+
+
+		$('#orderCategory').change(function(){
+			$('#search').keyup(function(){
+
+			var envio_filtro = $('#orderCategory').val();
+			var envio = $('#search').val();
+			$('#resultados').html('<section class="content"><div class="row"><div class="col-xs-12"><div class="box box-success"><div class="box-header"><h3 class="box-title">Libros</h3></div><div class="box-body"><table id="BooksTableOrder" class="table table-bordered table-hover"><thead><tr style="background:#E7FAE2;"><th>Libro</th><th>Autor</th><th>Editorial</th><th>Ejemplares</th><th>Disponibles</th><th>Información</th></tr></thead><tbody><div class="overlay"><i class="fa fa-refresh fa-spin"</i></div></tbody><tfoot><tr><th>Libro</th><th>Autor</th><th>Editorial</th><th>Ejemplares</th><th>Disponibles</th><th>Información</th></tr></tfoot></table></div></div></div></div></section>');
+
+			$.ajax({
+				type: "post",
+				url: '/user/search',
+				data: {
+								search: envio,
+								searchType: envio_filtro
+						 },
+				success: function(resp){
+					if(resp!=""){
+						$('#resultados').html(resp);
+					}
 				}
-			}
-		})
-	})
+			})
 
-	$('#search').keyup(function(){
-		var envio = $('#search').val();
-		$('#resultados').html('<section class="content"><div class="row"><div class="col-xs-12"><div class="box box-success"><div class="box-header"><h3 class="box-title">Libros</h3></div><div class="box-body"><table id="BooksTableOrder" class="table table-bordered table-hover"><thead><tr style="background:#E7FAE2;"><th>Libro</th><th>Autor</th><th>Editorial</th><th>Ejemplares</th><th>Disponibles</th><th>Información</th></tr></thead><tbody><div class="overlay"><i class="fa fa-refresh fa-spin"</i></div></tbody><tfoot><tr><th>Libro</th><th>Autor</th><th>Editorial</th><th>Ejemplares</th><th>Disponibles</th><th>Información</th></tr></tfoot></table></div></div></div></div></section>');
 
-		$.ajax({
-			type: "post",
-			url: '/user/search',
-			data: {
-            	search: envio
-       		 },
-			success: function(resp){
-				if(resp!=""){
-					$('#resultados').html(resp);
-				}
-			}
-		})
-	})
+
+				});
+			})
 
 
 
